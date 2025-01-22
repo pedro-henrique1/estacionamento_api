a64c7b52c269b3ae97f3e644a75f8b185dda6de0
Pedro Henrique Silva Rodrigues <ph8464803@gmail.com>
2025-01-22 19:14:53 -0300

    init

---
+++ src/test/java/backend/apiestacionamento/ApiEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.apiestacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEstacionamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
