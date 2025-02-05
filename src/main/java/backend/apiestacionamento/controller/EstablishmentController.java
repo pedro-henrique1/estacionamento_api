a20995e1c160980c70321d824715ff2a2c3f332b
Pedro Henrique Silva Rodrigues <ph8464803@gmail.com>
2025-02-05 17:14:52 -0300

    feat: criando rota e função para registro no banco do Establishment

--- src/main/java/backend/apiestacionamento/controller/EstablishmentController.java
+++ src/main/java/backend/apiestacionamento/controller/EstablishmentController.java
@@ -4,11 +4,9 @@ package backend.apiestacionamento.controller;
 import backend.apiestacionamento.dto.EstablishmentDto;
 import backend.apiestacionamento.service.EstablishmentService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController("/establishments")
 public class EstablishmentController {
 
     private final EstablishmentService establishmentService;
@@ -17,14 +15,32 @@ public class EstablishmentController {
         this.establishmentService = establishmentService;
     }
 
-
-
-
-    @PostMapping("/establishments/create")
+    @PostMapping
     public ResponseEntity<?> CreateEstablishment(@RequestBody EstablishmentDto establishment) {
         establishmentService.creatEstablishment(establishment);
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping
+    public ResponseEntity<?> getAllEstablishments() {
+        return ResponseEntity.ok(establishmentService.getAllEstablishments());
+    }
+
+    @GetMapping("${id}")
+    public ResponseEntity<?> getEstablishmentById(@PathVariable Long id) {
+        return ResponseEntity.ok(establishmentService.getEstablishmentById(id));
+    }
+
+    @PatchMapping
+    public ResponseEntity<?> updateEstablishment(@RequestBody EstablishmentDto establishment) {
+        establishmentService.updateEstablishment(establishment);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("${id}")
+    public ResponseEntity<?> deleteEstablishment(@PathVariable Long id) {
+        establishmentService.deleteEstablishmentById(id);
+        return ResponseEntity.ok().build();
+    }
 
 }
--- src/main/java/backend/apiestacionamento/service/EstablishmentService.java
+++ src/main/java/backend/apiestacionamento/service/EstablishmentService.java
@@ -3,9 +3,14 @@ package backend.apiestacionamento.service;
 
 import backend.apiestacionamento.dto.EstablishmentDto;
 import backend.apiestacionamento.dto.mapper.EstablishmentMapper;
+import backend.apiestacionamento.model.Establishment;
 import backend.apiestacionamento.repository.EstablishmentRepository;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EstablishmentService {
@@ -25,5 +30,31 @@ public class EstablishmentService {
             ResponseEntity.ok().build();
         }
         ResponseEntity.badRequest().build();
+    }
+
+    public List<EstablishmentDto> getAllEstablishments() {
+        List<Establishment> establishments = establishmentRepository.findAll();
+        return establishments.stream().map(establishmentMapper::establishmentToDTO).collect(Collectors.toList());
+    }
+
+    public EstablishmentDto getEstablishmentById(Long id) {
+        Establishment establishment = establishmentRepository.findById(id).orElseThrow(() -> new RuntimeException("establishment not found"));
+        return establishmentMapper.establishmentToDTO(establishment);
+    }
+
+
+    public EstablishmentDto updateEstablishment(EstablishmentDto establishment) {
+        Establishment establishment1 = establishmentRepository.findById(establishment.getId()).orElseThrow(() -> new RuntimeException("establishment not found"));
+        establishment.setName(establishment1.getName());
+        establishment.setCnpj(establishment.getCnpj());
+        establishment.setPhone(establishment.getPhone());
+        establishment.setVacancies_motorcycles(establishment.getVacancies_motorcycles());
+        establishment.setVacancies_car(establishment.getVacancies_car());
+        establishmentRepository.save(establishment1);
+        return establishmentMapper.establishmentToDTO(establishment1);
+    }
+
+    public void deleteEstablishmentById(Long id) {
+        establishmentRepository.deleteById(id);
     }
 }
