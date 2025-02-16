e9ffe5278ea54eb6723198e566c54d460a274371
Pedro Henrique Silva Rodrigues <ph8464803@gmail.com>
2025-02-16 18:10:20 -0300

    refactor: mudando tipo do id para integer

--- src/main/java/backend/apiestacionamento/repository/AddressesRepository.java
+++ src/main/java/backend/apiestacionamento/repository/AddressesRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddressesRepository extends JpaRepository<Addresses, Long> {
+public interface AddressesRepository extends JpaRepository<Addresses, Integer> {
 }
